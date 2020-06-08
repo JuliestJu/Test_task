e16829b6b780cfe3cfb62f36663cb1e46f16b389
JuliaVorotchenko <juliavorotchenko@gmail.com>
2020-06-08 11:00:46 +0300

    Preliminary project setup

---
+++ Anoda_Test_Task/Anoda_Test_Task/Anoda_Test_Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Anoda_Test_Task
+//
+//  Created by Юлия Воротченко on 08.06.2020.
+//  Copyright © 2020 Юлия Воротченко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .brown
+    }
+
+
+}
+
